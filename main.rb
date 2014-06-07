7c965d816dffae3639ca836bb812decbc49b8de6
Marcos Serpa <marcosserpa@gmail.com>
2014-06-07 12:14:26 -0300

    Parser

---
+++ main.rb
@@ -0,0 +1,7 @@
+#require 'parser'
+load 'parser.rb'
+require 'pry'
+
+class Main
+  binding.pry
+end
---
+++ parser.rb
@@ -0,0 +1,34 @@
+require 'csv'
+require 'pry'
+
+class CSVParser
+  #keys = [ :word_freq_make, :word_freq_address, :word_freq_all, :word_freq_3d, :word_freq_our, :word_freq_over, :word_freq_remove, 
+  #        :word_freq_internet, :word_freq_order, :word_freq_mail, :word_freq_receive, :word_freq_will, :word_freq_people, :word_freq_report, 
+  #        :word_freq_addresses, :word_freq_free, :word_freq_business, :word_freq_email, :word_freq_you, :word_freq_credit, :word_freq_your, 
+  #        :word_freq_font, :word_freq_000, :word_freq_money, :word_freq_hp, :word_freq_hpl, :word_freq_george, :word_freq_650, :word_freq_lab, 
+  #        :word_freq_labs, :word_freq_telnet, :word_freq_857, :word_freq_data, :word_freq_415, :word_freq_85, :word_freq_technology, 
+  #        :word_freq_1999, :word_freq_parts, :word_freq_pm, :word_freq_direct, :word_freq_cs, :word_freq_meeting, :word_freq_original, 
+  #        :word_freq_project, :word_freq_re, :word_freq_edu, :word_freq_table, :word_freq_conference, 
+  #        :char_freq_semicolon, #;
+  #        :char_freq_open_parenthesis, #(
+  #        :char_freq_open_brackets, #[
+  #        :char_freq_!,
+  #        :char_freq_dollar, #$
+  #        :char_freq_number, ##
+  #        :capital_run_length_average, :capital_run_length_longest, :capital_run_length_total, :spam ]
+
+  #keys2 = [ "chave1", "chave2" ]
+
+  # TODO read name file from method parameter
+  def self.csv_to_hash(file_name = '')
+    if file_name.empty?
+      raise "Give the name of .csv."
+    end
+
+    body = File.read(file_name)
+
+    csv = CSV.new(body, :headers => true, :header_converters => :symbol)
+    csv.to_a.map! {|row| row.to_hash }
+  end
+
+end
