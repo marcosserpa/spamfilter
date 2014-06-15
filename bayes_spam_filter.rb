5bd1fe8a92f66e39aedc2da07b793e6d46e0483c
Marcos Serpa <marcosserpa@gmail.com>
2014-06-15 10:29:44 -0300

    Training preparation ended.

--- bayes_spam_filter.rb
+++ bayes_spam_filter.rb
@@ -22,75 +22,38 @@ class BayesSpamFilter
     # csv = File.read('spambase_2.csv')
     # spam_base = CSV.new(csv, :converters => :all)
     # spam_base_array = spam_base.to_a
-    puts "Type the the CSV file name with the extension:"
+    puts "Type the CSV file name with the extension:"
     file_name = gets.chomp
 
     CSVParser.csv_to_hash(file_name)
   end
 
+  # Training by Cross-Validation Technique - 2 randomly set sets
   #This function takes a number, and separates it from the total groups, creating a testing group and a training group.
   #Takes a set number to be excluded (iteration wise) and passed 2 lists : testing group and training group to the training function.
   #Based on the input option, calls the appropriate function.
   #contract -> (int,list[list],int) returns (void) ,like a main function for the entire 3 methods. calls the related functions with corresponding parameters.
-  def self.trainFolds(excludeSet,groups,option)
-    trainingGroup = []
-  	testingGroup = groups[excludeSet] # exclude the testing group
+  def self.train_algorithm(hash, training_hashes_size, distribuction_option)
+    training_hashes_indexes = training_hashes_size.times.map{ 0 + rand(4601) }
+    testing_hashes = hash.clone
+    training_hashes = {}
 
-  	# for j in range(0,len(groups))
-  	# 	if j != excludeSet:
-  	# 		trainingGroup.append(groups[j])
+    # separates training set from testing set
+    training_hashes_indexes.each do |index|
+      training_hashes[index] = testing_hashes.delete(index)
+    end
 
-  	if option == 1
+    # training result by the method selected
+  	if distribuction_option == 1
   		print "Results for Fold " , excludeSet
-  		training(testingGroup,trainingGroup)                    #Runs Bernoulli Method.
-  	elsif option == 2
+  		training(testingGroup,trainingGroup)  #Runs Bernoulli Method.
+  	elsif distribuction_option == 2
   		print "Results for Fold " , excludeSet
   		gaussianMethod(testingGroup,trainingGroup)	#Runs Gaussian Method.
-  	elsif option ==3
+  	elsif distribuction_option ==3
   		print "Results for Fold " , excludeSet
   		histogramMethod(testingGroup,trainingGroup) #Runs Histogram Method.
     end
   end
 
 end
-
-
-
-
-
-
-
-
-# class Bayes
-# # creation of k fold
-#
-#
-# #reads the spambase file and creates the k folds.
-# #input is nothing, output are the groups.
-# #contract -> (void) returns (list[list])
-# def self.getKFolds
-#   #gets all the data from the spambase file and stores it in a dictionary
-#   spam_base = File.read('spambase.data')
-#   binding.pry
-# # 	spambaseData = open('spambase.data','r').readlines()
-# # 	spambase = {}
-# # 	i = 1
-# # 	for spamfile in spambaseData:
-# # 		spambase[i] = spamfile.strip()
-# # 		i += 1
-# # #breaks the spambase dictonary into smaller chunks of sort {1,11,22,...} uptil {10,20,30,...}
-# # 	groups = []
-# # 	for k in range(1,11):
-# # 		fold = []
-# # 		fold.append(spambase[k])
-# # 		j=k
-# # 		while(j<4599):
-# # 			if(j+10>4601):
-# # 				break
-# # 			else:
-# # 				fold.append(spambase[j+10])
-# # 				j += 10
-# # 		groups.append(fold)
-# # 	return groups
-# end
-# end
--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ load 'bayes_spam_filter.rb'
 #Kicks off the process of training and testing for the 10 folds by first creating the folds.
 #validates the input and sends parameters to functions.
 class Main
+
   #fetures = [ 'word_freq_make', :word_freq_address, :word_freq_all, :word_freq_3d, :word_freq_our, :word_freq_over, :word_freq_remove,
   #        :word_freq_internet, :word_freq_order, :word_freq_mail, :word_freq_receive, :word_freq_will, :word_freq_people, :word_freq_report,
   #        :word_freq_addresses, :word_freq_free, :word_freq_business, :word_freq_email, :word_freq_you, :word_freq_credit, :word_freq_your,
@@ -21,13 +22,14 @@ class Main
   #        :char_freq_number, ##
   #        :capital_run_length_average, :capital_run_length_longest, :capital_run_length_total, :spam ]
 
-  # returns the content of the CSV as hash of hashs. Each line is a hash
+  # Returns the content of the CSV as hash of hashs. Each line is a hash
   hash = BayesSpamFilter.getSampleHashs #CSVParser.csv_to_hash
+  training_hashes_size = (hash.size * 0.8).to_int
 
   puts "Choose the method:\n Type\n 1 to use the Bernoulli method\n 2 to use the Gaussian method and\n 3 to use Histogram Method."
   option = gets.chomp
 
-  # user chooses the distribuction to generate the results
+  # User chooses the distribuction to generate the results
 	if option != '1' and option != '2' and option != '3'
 	  print("You need to enter 1, 2 or 3.")
 	elsif option == '1'
@@ -38,10 +40,7 @@ class Main
 		print "Using the Histogram Distribution to generate the results"
   end
 
-  binding.pry
-
-	# for excludeSet in range(0,len(groups)):
-  # #for excludeSet in range(0,1):   #runs only one set.
-	# 	trainFolds(excludeSet,groups,option)
+  # Trains the algorithm
+  BayesSpamFilter.train_algorithm(hash, training_hashes_size, distribuction_option)
 
 end
