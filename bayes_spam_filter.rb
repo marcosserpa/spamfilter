cb9097f36860b9800a26c6e4c79c507c31e377fc
Marcos Serpa <marcosserpa@gmail.com>
2014-06-15 16:34:02 -0300

    Averages calcs.

--- bayes_spam_filter.rb
+++ bayes_spam_filter.rb
@@ -12,6 +12,21 @@ load 'parser.rb'
 
 class BayesSpamFilter
 
+  features = [ :word_freq_make, :word_freq_address, :word_freq_all, :word_freq_3d, :word_freq_our, :word_freq_over, :word_freq_remove,
+               :word_freq_internet, :word_freq_order, :word_freq_mail, :word_freq_receive, :word_freq_will, :word_freq_people, :word_freq_report,
+               :word_freq_addresses, :word_freq_free, :word_freq_business, :word_freq_email, :word_freq_you, :word_freq_credit, :word_freq_your,
+               :word_freq_font, :word_freq_000, :word_freq_money, :word_freq_hp, :word_freq_hpl, :word_freq_george, :word_freq_650, :word_freq_lab,
+               :word_freq_labs, :word_freq_telnet, :word_freq_857, :word_freq_data, :word_freq_415, :word_freq_85, :word_freq_technology,
+               :word_freq_1999, :word_freq_parts, :word_freq_pm, :word_freq_direct, :word_freq_cs, :word_freq_meeting, :word_freq_original,
+               :word_freq_project, :word_freq_re, :word_freq_edu, :word_freq_table, :word_freq_conference,
+               :char_freq_semicolon, #;
+               :char_freq_open_parenthesis, #(
+               :char_freq_open_brackets, #[
+               :char_freq_!,
+               :char_freq_dollar, #$
+               :char_freq_number, ##
+               :capital_run_length_average, :capital_run_length_longest, :capital_run_length_total, :spam ]
+
   # creation of k fold
   #reads the spambase file and creates the k folds.
   #input is nothing, output are the groups.
@@ -46,14 +61,65 @@ class BayesSpamFilter
     # training result by the method selected
   	if distribuction_option == 1
   		print "Results for Fold " , excludeSet
-  		training(testingGroup,trainingGroup)  #Runs Bernoulli Method.
+  		bernoulli_distribuction(training_hashes, testing_hashes)  #Runs Bernoulli Method.
   	elsif distribuction_option == 2
   		print "Results for Fold " , excludeSet
-  		gaussianMethod(testingGroup,trainingGroup)	#Runs Gaussian Method.
+  		gaussianMethod(training_hashes, testing_hashes)	#Runs Gaussian Method.
   	elsif distribuction_option ==3
   		print "Results for Fold " , excludeSet
-  		histogramMethod(testingGroup,trainingGroup) #Runs Histogram Method.
+  		histogramMethod(training_hashes, testing_hashes) #Runs Histogram Method.
     end
+  end
+
+  #This function takes in the training and testing sets and performs the training/testing operations for bernoulli classifier and calcuates the error rate,tpr,fpr by calling helper functions.
+  #input is the testing group and the training group of emails.
+  #prints the error rate,false positive rate, false negative rate, and the area under the curve for a particular fold.
+  #contract -> (list,list) returns (void), like the main function for the bernoulli method. Works by calling helper functions.
+  def self.bernoulli_distribuction(training_set, testing_set):
+  	$tprList
+  	tprList = []
+  	$fprList
+  	fprList = []
+
+  	spam_averages, not_spam_averages, spam_counter, not_spam_counter = calculateMeanValues(training_set)
+  	meanValues = calculateGaussianMean(trainEmail)
+  	probabilitiesCaseI,probabilitiesCaseII,probabilitiesCaseIII,probabilitiesCaseIV = normalizeTrainingSet(trainEmail,meanValues,spamCount,notSpamCount)
+  	resultDict,scoreList = bernoulliCalculation(testingGroup,meanValues,probabilitiesCaseI,probabilitiesCaseII,probabilitiesCaseIII,probabilitiesCaseIV,spamCount,notSpamCount)
+  	errorRate,actualDict,falsePositiveRate,falseNegativeRate = calculateErrorRate(testingGroup,resultDict)
+  	getROCPoints(scoreList,actualDict,testingGroup)
+  end
+
+  # Calculates the conditional average values for the features in the training set spam/not spam emails.
+  # Input is the training set and returns the mean values hash and a list of emails.
+  # Helper function called by all the 3 classifiers.
+  # contract -> (list) returns (dict, dict, int,int)
+  def self.calculates_averages(training_set):
+  	train_email = []
+  	spam_averages = {}
+  	not_spam_averages = {}
+
+    features.each do |feature|
+      spam_counter = 0
+      not_spam_counter = 0
+      spam_feature_average = 0.0
+      not_spam_feature_average = 0.0
+
+      training_set.each do |email|
+        if email[:spam] == 1
+          spam_counter += 1
+          spam_feature_average += email[feature]
+        else
+          not_spam_counter += 1
+          not_spam_feature_average += email[feature]
+        end
+      end
+
+      # Store the feature averages to spam and not spam mails
+      spam_averages[feature] = spam_feature_average / spam_counter
+      not_spam_averages[feature] = not_spam_feature_average / not_spam_counter
+    end
+
+    return [spam_averages, not_spam_averages, spam_counter, not_spam_counter]
   end
 
 end
